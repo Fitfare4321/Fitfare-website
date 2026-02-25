39f6ef34338f003402f9bad72a323ec1abcdc115
pratapmajge <majgepratap1504@gmail.com>
2026-02-25 22:59:10 +0530

    Number changed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,8 +5,7 @@ import CountUp from "react-countup";
 import { useInView } from "react-intersection-observer";
 import { useTheme } from "next-themes";
 
-import heroVideo from "@/assets/allvideos-hero.mp4";
-
+import heroVideo from "@/assets/s.mp4";
 
 /* Floating Particles */
 const FloatingParticle = ({
